f2357fa174cf6ccad7c44acf09ff5d9439736f70
Romancevic <Romancevic2@gmail.com>
2021-04-11 13:09:49 +0100

    Add files via upload

---
+++ libs/php/config.php
@@ -0,0 +1,8 @@
+<?php
+    $cd_host = "127.0.0.1";
+    $cd_user = "romancevic_test";
+    $cd_password = "unV%q!!X4k";
+    $cd_dbname = "romancevic_test";
+    $cd_port = "3306";
+    $cd_socket = "";    
+?>
